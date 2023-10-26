d924777da882b24191a852d4a36723e28bacb683
Vilhelms <vilhelmsbavins123@gmail.com>
2023-10-26 18:00:54 +0300

    Added new button

--- js/button.js
+++ js/button.js
@@ -8,8 +8,27 @@ function newHex() {
     return randomInt() + ', ' + randomInt() + ', ' + randomInt() + ', '
 }
 
-// Funkcija nomaina otru (apakšējo) krāsu fona attēlam
+// Funkcija nomaina lapas apakšējās daļas krāsu
 function changeGradient() {
     var gradientBg = document.getElementById("gradiant-bg");
     gradientBg.style.background = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.75), rgba(' + newHex() + '0.5))';
+}
+
+// Funkcija maina lapas apakšējās daļas krāsu ik pēc 1 sekundes
+var isToggled = false;
+function toggleChangeGradient() {
+    isToggled = !isToggled;
+    var toggleButton = document.getElementById("toggleButton");
+    if (isToggled) {
+        toggleButton.textContent = "Izslēgt krāsu maiņu";
+        var intervalId = setInterval(changeGradient, 1000);
+        toggleButton.onclick = function() {
+            isToggled = false;
+            clearInterval(intervalId);
+            toggleButton.textContent = "Ieslēgt krāsu maiņu";
+            toggleButton.onclick = toggleChangeGradient;
+        };
+    } else {
+        toggleButton.textContent = "Ieslēgt krāsu maiņu";
+    }
 }
